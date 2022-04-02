d8b8ac449f9b5948acb2fa4b04250dd428f2ecc9
bharvichavda <90469752+bharvichavda@users.noreply.github.com>
2022-04-02 16:31:43 +0530

    Add files via upload

---
+++ Py1.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[24]:
+
+
+Name = " Bharvi"
+Department = 'Statistics'
+Github = 'bharvichavda'
+
+
+# In[26]:
+
+
+x = "abc"
+def f() :
+    x = "Bharvi"
+    print("My name is",x)
+    print(x)
+f()
+print(x)
+
+
+# In[25]:
+
+
+b = 3    # 3 is a number
+c = "3"  # 3 is a string
+
+
+# In[9]:
+
+
+# Single line comment
+# Multiple line comments
+""" msnxjkwehifuheiou93
+kdnkj3
+nek3n
+"""
+
+
+# In[13]:
+
+
+# to declare variable
+# variable starts from A-Z, a-z
+# We can't puut keywords in python as a variable except _
+# Python is a case sensitive language
+A_ = 1
+a_ = 2
+a = 3
+A = 4
+print(A_)
+print(a_)
+print(A)
+print(a)
+
+
+# In[18]:
+
+
+# Python as A Calculator
+x, y = 10, 5
+print("Addition=",x+y)
+print("Subtraction=",x-y)
+print("Multiplication=",x*y)  
+print("Division=",x/y)
+print("Power=",x**y)
+print("Modulus=",x%y)
+print("Floor division=",x//y)
+
+
+# In[19]:
+
+
+#Assigning Multiple Values to a Variable
+x, y, z,a,b,c,f = "Orange", "Banana", "Cherry",241,"fwdfwe","3235","sdasd"
+print(x)
+print(y)
+print(z)
+print(a)
+print(b)
+print(c)
+
